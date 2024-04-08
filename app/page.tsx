35224b86e7f187ee149b8610c7a41c28f70cd9a1
cho-hyeonjin <gusls2005@gmail.com>
2024-04-09 00:29:10 +0900

    fix: build 에러

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Footer from "@/components/footer/footer";
 import Hero from "@/components/hero/hero";
 import Navbar from "@/components/nav-bar/nav-bar";
-import { NAV_BAR_ITEMS } from "@/constants";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-[100px] pb-10">
       <Navbar />
       <Hero />
-      <Footer links={NAV_BAR_ITEMS} />
+      <Footer />
     </div>
   );
 }
